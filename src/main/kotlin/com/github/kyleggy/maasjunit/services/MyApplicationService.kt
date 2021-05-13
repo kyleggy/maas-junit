6be945b40767821c080fd3a041ce915447b6c862
GitHub Action <action@github.com>
2021-05-13 03:55:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/kyleggy/maasjunit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kyleggy/maasjunit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kyleggy.maasjunit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kyleggy.maasjunit.MyBundle
 
 class MyApplicationService {
 
